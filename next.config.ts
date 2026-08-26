f9a99f141e43518fe3343522a9e2d1aeba15b442
Appon Islam <appon2003@gmail.com>
2026-08-26 11:24:36 +0600

    optimization added

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: "export",
+    reactStrictMode: true,
     images: {
         unoptimized: true,
     },
     transpilePackages: ["react-icons"],
+    experimental: {
+        optimizePackageImports: ["lucide-react", "react-icons", "@radix-ui/react-icons"],
+    },
 };
 
 export default nextConfig;
